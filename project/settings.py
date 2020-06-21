29f683b0efed86dc15560ea5aab88cd255585a92
Matt Layman <matthewlayman@gmail.com>
2020-06-21 13:53:24 -0400

    It usually helps when you don't revert the setting you added.

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 LOGGING = {
     "version": 1,
